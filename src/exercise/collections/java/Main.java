472488e23cdf86cbf4124145d2886ad7f027d492
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-23 12:32:21 -0500

    testing main method

---
+++ src/exercise/collections/java/Main.java
@@ -0,0 +1,10 @@
+package exercise.collections.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Number of strings is " + args.length);
+        for (int i = 0; i < args.length; i++)
+            System.out.println(args[i]);
+    }
+}
